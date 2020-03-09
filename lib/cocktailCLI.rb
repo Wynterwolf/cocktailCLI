4fa79203d416ef725ac09888bfcfc42f90d7e15d
wynterwolf <shinyguns@gmail.com>
2020-03-09 09:27:26 -0500

    first commit

---
+++ lib/cocktailCLI.rb
@@ -0,0 +1,6 @@
+require "cocktailCLI/version"
+
+module CocktailCLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cocktailCLI/version.rb
@@ -0,0 +1,3 @@
+module CocktailCLI
+  VERSION = "0.1.0"
+end

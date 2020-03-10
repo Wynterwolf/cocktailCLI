c1ef2e2563f0beb199433b5af7eaa9d5a3795711
wynterwolf <shinyguns@gmail.com>
2020-03-10 14:52:29 -0500

    two searches work

--- lib/cocktailCLI/api.rb
+++ lib/cocktailCLI/api.rb
@@ -6,12 +6,25 @@ def self.fetch(term)
     response = HTTParty.get(url)
 
     response["drinks"].each do |drink|
-        #drink = drink["strDrink"]
+        drink = drink["strDrink"]
         puts drink["strDrink"]
-        #CocktailCLI::Drink.new(drink)
+        CocktailCLI::Drink.new(drink)
 
 
         #binding.pry
     end
 end
+
+def self.fetch_two(term)
+    key = "1"
+    url = "https://www.thecocktaildb.com/api/json/v1/#{key}/search.php?f=#{term}"
+    response = HTTParty.get(url)
+
+    response["drinks"].each do |drink|
+        drink = drink["strDrink"]
+        puts drink["strDrink"]
+        CocktailCLI::Drink.new(drink)
+    end
+end
+
 end
--- lib/cocktailCLI/cli.rb
+++ lib/cocktailCLI/cli.rb
@@ -28,10 +28,22 @@ class CocktailCLI::CLI
         #something here to make an object? drinks.rb?
         term = gets.chomp.downcase #<--- this?
         CocktailCLI::API.fetch(term)  #<---this?
+        
+        CocktailCLI::Drink.all.each.with_index(1) do |d, index|
+            puts "#{index}. #{d.drink}"
+            puts "--------------------------------------------"
+        end
     when "2"
         puts "You chose to search by letter..."
         puts "--------------------------------------------"
         puts "Please choose a letter ..."
+        term = gets.chomp.downcase
+        CocktailCLI::API.fetch_two(term)
+
+        CocktailCLI::Drink.all.each.with_index(1) do |d, index|
+            puts "#{index}. #{d.drink}"
+            puts "--------------------------------------------"
+        end
         # can I put a case in a case? or should this be ?
         #something else to make an object
     when "3"

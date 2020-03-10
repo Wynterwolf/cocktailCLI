8c3762e525bb86f41e28877c7a7a46c8b6ccbe80
wynterwolf <shinyguns@gmail.com>
2020-03-10 12:30:03 -0500

    added choice case statement

--- lib/cocktailCLI/cli.rb
+++ lib/cocktailCLI/cli.rb
@@ -8,18 +8,36 @@ class CocktailCLI::CLI
         puts "Hello #{name}, welcome to my CocktailCLI!"
         puts "--------------------------------------------"
         sleep(2)
-        puts "You can search by drink name!"
+        puts "(1)You can search by drink name!"
         sleep(2)
         puts "--------------------------------------------"
-        puts "You can search by letter!"
+        puts "(2)You can search by letter!"
         sleep(2)
         puts "--------------------------------------------"
-        puts "You can search by ingredient!"
+        puts "(3)You can search by ingredient!"
         puts "--------------------------------------------"
         sleep(2)
+        puts "Which would you like to do, choose a number please...."
+        choice = gets.chomp
+
+    case choice
+    when 1
         puts "You chose to search by drink name..."
         puts "--------------------------------------------"
         puts "What drink recipe would you like to see?"
+    when 2
+        puts "You chose to search by letter..."
+        puts "--------------------------------------------"
+        puts "Please choose a letter ..."
+        # can I put a case in a case? or should this be ?
+    when 3
+        puts "You chose to search by ingredient..."
+        puts "--------------------------------------------"
+        puts "Please enter an ingredient ..."
+        # what about this?
+    else 
+        exit
+    end
 
         term = gets.chomp.downcase
         CocktailCLI::API.fetch(term)
